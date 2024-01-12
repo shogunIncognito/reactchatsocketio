20717aa302a2121b0cfa976f9655eb7c7dcb4bff
BeiGod <saidsamuel2006@hotmail.com>
2024-01-11 20:52:18 -0500

    Refactor Live Chat UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
 
   useEffect(() => {
     socket.on('message', message => setMessages(prev => [...prev, message]))
-    socket.on('typing', username => 
+    socket.on('typing', username =>
       setTyping(prev => [...new Set([...prev, username])])
     )
     socket.on('stopTyping', username => {
@@ -57,27 +57,28 @@ export default function App() {
 
   return (
     <div className='h-screen flex-col bg-neutral-950 flex justify-center items-center'>
-      <div className='bg-slate-800 max-h-[80%] md:max-h-[50%] md:max-w-[50%] max-w-[80%] gap-3 md:w-1/2 md:h-1/2 w-4/5 h-4/5 flex flex-col p-3 rounded-md'>
-        <h2 className='text-white text-center text-2xl'>Live Chat</h2>
-        <input
-          type="text"
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className='p-1 rounded w-1/3 bg-transparent border-b border-white focus:border-blue-400 transition-colors text-white outline-none'
-        />
-        
+      <h2 className='text-white text-center text-2xl md:text-4xl mb-5'>Live Chat</h2>
+      <h2 className='text-white'>¿Whats your username?</h2>
+      <input
+        type="text"
+        placeholder='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className='p-1 rounded w-1/6 mb-5 mt-2 text-center bg-transparent border-b border-white focus:border-blue-400 transition-colors text-white outline-none'
+      />
+
+      <div className='bg-slate-900 max-h-[80%] md:max-h-[66%] md:max-w-[50%] max-w-[80%] gap-3 md:w-1/2 md:h-2/3 w-4/5 h-4/5 p-2 flex flex-col rounded-md'>
         <Messages messages={messages} />
 
         <div>
           {typing.length > 0 && (
-            <small className='text-gray-400'>
+            <small className='text-gray-400 ml-5'>
               {typing.length === 1 ? `${typing[0]} is typing...` : `${typing.join(', ')} are typing...`}
-            </small>          
+            </small>
           )}
         </div>
-        
-        <form onSubmit={handleSubmit} className='flex gap-2'>
+
+        <form onSubmit={handleSubmit} className='flex gap-2 p-3'>
           <input
             type="text"
             value={message}
@@ -86,7 +87,7 @@ export default function App() {
             onChange={handleMessageChange}
             className='p-1 rounded w-full bg-transparent border-b border-white focus:border-blue-400 transition-colors text-white outline-none'
           />
-          <button className='p-1 px-4 bg-green-600 text-white rounded'>Send</button>
+          <button className='p-1 px-4 bg-green-600 hover:bg-green-800 transition-colors text-white rounded'>Send</button>
         </form>
       </div>
     </div>
